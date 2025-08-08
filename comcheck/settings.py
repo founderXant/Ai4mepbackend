d71144cc8171808cf4194279871548da48c176a9
founderXant <anthony@ai4mep.com>
2025-08-07 20:56:51 -0400

    -removed app files from comcheck folder -changed settings.py to allow older django ver. -secret key

--- comcheck/settings.py
+++ comcheck/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@)he8nt@^ybps6g@nhn6b^))0x_z0#fum5pp*z6+u%p68*942'
+SECRET_KEY = 'SECRET_KEY_PLACEHOLDER'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
